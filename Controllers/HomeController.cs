931d2279bc93177af4b502477ba642ab5c958bf5
Ivan <molofeev.ivan@bk.ru>
2023-03-20 22:23:20 +0400

    1
    1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using LABA.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        var model = new CalculatorViewModel();
+        return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult Calculate(CalculatorViewModel model, string submitButton)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View("Index", model);
+        }
+        if (submitButton == "Clear")
+        {
+            model.Result = 0;
+            return View("Index", model);
+        }
+
+        switch (model.Operator)
+        {
+            case "+":
+                model.Result = model.FirstOperand + model.SecondOperand;
+                break;
+            case "-":
+                model.Result = model.FirstOperand - model.SecondOperand;
+                break;
+            case "*":
+                model.Result = model.FirstOperand * model.SecondOperand;
+                break;
+            case "/":
+                model.Result = model.FirstOperand / model.SecondOperand;
+                break;
+
+        }
+
+        ViewBag.TargetResult = 42.0m;
+
+        return View("Index", model);
+    }
+
+}
---
+++ Models/CalculatorViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace LABA.Models
+{
+    public class CalculatorViewModel
+    {
+        [Required(ErrorMessage = "The first operand is required.")]
+        public ushort FirstOperand { get; set; }
+
+        public string? Operator { get; set; }
+
+        [Range(1, 100, ErrorMessage = "The second operand must be between {1} and {2}.")]
+        public ushort SecondOperand { get; set; }
+
+        public decimal Result { get; set; }
+        public string[] Operators { get; }
+
+        public CalculatorViewModel()
+        {
+            Operators = new[] { "+", "-", "*", "/" };
+        }
+    }
+}
