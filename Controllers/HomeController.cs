723b11ab6f0032d2ee34e2213435144a9244784f
Ivan <molofeev.ivan@bk.ru>
2023-03-21 09:22:48 +0400

    ..

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 ﻿using LABA.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class HomeController : Controller
 {
@@ -40,8 +44,30 @@ public class HomeController : Controller
         }
 
         ViewBag.TargetResult = 42.0m;
-
+        CookieOptions option = new CookieOptions();
+        option.Expires = DateTime.Now.AddMinutes(10);
+        Response.Cookies.Append("OperationInfo", model.FirstOperand.ToString() + model.Operator.ToString() + model.SecondOperand.ToString() + " = " + model.Result.ToString(), option);
+        // Возвращаем представление Index.cshtml
         return View("Index", model);
+        
+    }
+
+    public IActionResult Equation()
+    {
+        if (Request.Cookies["OperationInfo"] != null)
+        {
+            string result = Request.Cookies["OperationInfo"];
+            int index = result.LastIndexOfAny(new char[] { '=' });
+            string resultInfo = result.Remove(index, result.Length - index) + " равно " + result.Substring(index + 1);
+            ViewBag.Equat = resultInfo;
+            Console.WriteLine(resultInfo);
+        }
+        return View();
+
+    }
+    public IActionResult Result()
+    {
+        return View("Result", ViewBag.Equat);
     }
 
 }
--- Models/CalculatorViewModel.cs
+++ Models/CalculatorViewModel.cs
@@ -6,19 +6,26 @@ namespace LABA.Models
 {
     public class CalculatorViewModel
     {
+        // Первый операнд
         [Required(ErrorMessage = "The first operand is required.")]
         public ushort FirstOperand { get; set; }
 
+        // Оператор
         public string? Operator { get; set; }
 
+        // Второй операнд
         [Range(1, 100, ErrorMessage = "The second operand must be between {1} and {2}.")]
         public ushort SecondOperand { get; set; }
 
+        // Результат операции
         public decimal Result { get; set; }
+
+        // Массив доступных операторов
         public string[] Operators { get; }
 
         public CalculatorViewModel()
         {
+            // Инициализация массива доступных операторов
             Operators = new[] { "+", "-", "*", "/" };
         }
     }
